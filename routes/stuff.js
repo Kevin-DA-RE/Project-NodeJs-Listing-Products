62e70a580f6749ef6bec47f9b820789fabc031be
Kevin DA-RE <dare.kevin21000@gmail.com>
2023-02-08 22:36:07 +0100

    Ajout des controlleurs et routers

---
+++ routes/stuff.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const stuffCtrl = require('../controllers/stuff');
+const router = express.Router();
+
+router.get('/', stuffCtrl.getAllThing);
+router.post('/', stuffCtrl.createThing);
+router.get('/:id', stuffCtrl.getOneThing);
+router.put('/:id', stuffCtrl.modifyThing);
+router.delete('/:id', stuffCtrl.deleteThing);
+
+
+
+module.exports = router;
